a194e9c0731c0ddce0aaff7ac4475d72c1990550
lucaspaszinski <lucaspaszinski@gmail.com>
2020-06-28 17:56:15 -0300

    Create New user funcionality added.
    Routes changed from 'get' to 'resources'.
    
    Added New user form.
    
    Accounts context new functions.
    
    User changeset

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -2,6 +2,11 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
   alias Rumbl.Accounts
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%Accounts.User{})
+    render(conn, "new.html", changeset: changeset)
+  end
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
@@ -10,5 +15,12 @@ defmodule RumblWeb.UserController do
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    {:ok, user} = Accounts.create_user(user_params)
+    conn
+    |> put_flash(:info, "#{user.name}, your user has been created!")
+    |> redirect(to: Routes.user_path(conn, :index))
   end
 end
