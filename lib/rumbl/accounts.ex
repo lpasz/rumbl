af6e527286e7b408d152efcb963e3a7a0f3e58f1
lucaspaszinski <lucaspaszinski@gmail.com>
2020-06-28 10:07:46 -0300

    Finished Accounts context.

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -1,16 +1,30 @@
-defmodule Accounts do
+defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context
   """
 
+  # Allow use of User instead of full context
   alias Rumbl.Accounts.User
 
   def list_users do
     [
-      %User{  id: "1", name: "José" ,  username: "josevalim"      },
-      %User{  id: "2", name: "Chris",  username: "chrismccords"   },
-      %User{  id: "3", name: "Joe"  ,  username: "joearmstrong"   },
-      %User{  id: "4", name: "Lucas",  username: "lucaspaszinski" },
+      %User{id: "1", name: "José" , username: "josevalim"     },
+      %User{id: "2", name: "Chris", username: "chrismccords"  },
+      %User{id: "3", name: "Joe"  , username: "joearmstrong"  },
+      %User{id: "4", name: "Lucas", username: "lucaspaszinski"}
     ]
+  end
+
+  def get_user(id) do
+    list_users()
+    |> Enum.find(fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn user ->
+      Enum.all?(
+        params,
+        fn {key, value} -> Map.get(user, key) == value end)
+    end)
   end
 end
