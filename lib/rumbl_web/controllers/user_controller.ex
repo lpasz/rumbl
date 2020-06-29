8608b31abfc5f0a1a4d309c9f6721e5b293f25fc
lucaspaszinski <lucaspaszinski@gmail.com>
2020-06-29 20:49:47 -0300

    Redirect user of not logged in.

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -9,8 +9,14 @@ defmodule RumblWeb.UserController do
   end
 
   def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.html", users: users)
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+
+      conn ->
+        users = Accounts.list_users()
+        render(conn,"index.html", users: users)
+    end
   end
 
   def show(conn, %{"id" => id}) do
@@ -30,5 +36,14 @@ defmodule RumblWeb.UserController do
     end
   end
 
-  
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
