9762c6ab758ed1ac2076cf145b97b4c8f0b048dc
lucaspaszinski <lucaspaszinski@gmail.com>
2020-07-01 20:21:38 -0300

    Added belongs to to create relation

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -2,13 +2,13 @@ defmodule Rumbl.Multimedia.Video do
   use Ecto.Schema
   import Ecto.Changeset
 
-  
+
 
   schema "videos" do
     field :description, :string
     field :title, :string
     field :url, :string
-    field :user_id, :id
+    belongs_to :user, Rumbl.Accounts.User
 
     timestamps()
   end
