5dd04710dd96658c41cee668c74c9fa977ace814
lucaspaszinski <lucaspaszinski@gmail.com>
2020-07-19 21:22:38 -0300

    Assoc contrains added for category

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -2,8 +2,6 @@ defmodule Rumbl.Multimedia.Video do
   use Ecto.Schema
   import Ecto.Changeset
 
-
-
   schema "videos" do
     field :description, :string
     field :title, :string
@@ -20,5 +18,6 @@ defmodule Rumbl.Multimedia.Video do
     video
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
   end
 end
