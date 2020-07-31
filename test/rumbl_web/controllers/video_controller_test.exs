4655b0709f4d6c2999f3d073387c6698c344ba1a
Lucas Paszinski <lucaspaszinski@gmail.com>
2020-07-30 21:28:12 -0300

    Added test for valid and invalid video creation.

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -29,7 +29,8 @@ defmodule RumblWeb.VideoControllerTest do
       {:ok, conn: conn, user: user}
     end
 
-    @tag login_as: "max" # Need the specified tag for work
+    # Need the specified tag for work
+    @tag login_as: "max"
     test "list all user's videos on index", %{conn: conn, user: user} do
       user_video =
         video_fixture(
@@ -47,6 +48,48 @@ defmodule RumblWeb.VideoControllerTest do
       assert html_response(conn, 200) =~ ~r/Listing Videos/
       assert String.contains?(conn.resp_body, user_video.title)
       refute String.contains?(conn.resp_body, other_video.title)
+    end
+
+    alias Rumbl.Multimedia
+
+    @create_attrs %{
+      url: "http://youtu.be",
+      title: "the vid",
+      description: "the goddam vid"
+    }
+
+    @invalid_attrs %{
+      title: "invalid"
+    }
+
+    defp video_count, do: Enum.count(Multimedia.list_videos())
+
+    @tag login_as: "max"
+    test "create user then video and redirects", %{conn: conn, user: user} do
+      # post/create new video
+      create_conn = post conn, Routes.video_path(conn, :create), video: @create_attrs
+      # assert has an id
+      assert %{id: id} = redirected_params(create_conn)
+      # asser that you have denn redirected to show the video
+      assert redirected_to(create_conn) == Routes.video_path(create_conn, :show, id)
+      # get show the video with specific id
+      conn = get(conn, Routes.video_path(conn, :show, id))
+      # assert html page is show video
+      assert html_response(conn, 200) =~ "Show Video"
+      # Check if user if is the same of the user that created the video
+      assert Multimedia.get_video!(id).user_id == user.id
+    end
+
+    @tag login_as: "max"
+    test "does not create vid, renders error when invalid", %{conn: conn} do
+      # Get the number of videos of user before operation
+      count_before = video_count()
+      # Try to create a video, but with invalid params
+      conn = post conn, Routes.video_path(conn, :create), video: @invalid_attrs
+      # Assert html reponse was valid, but show the errors
+      assert html_response(conn, 200) =~ "check the errors"
+      # Confirmes that no video was actually added
+      assert video_count == count_before
     end
   end
 end
