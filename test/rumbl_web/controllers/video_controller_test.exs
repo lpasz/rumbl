0f72c53462728aa7f1223124ef72a7a7d5d6c5cc
Lucas Paszinski <lucaspaszinski@gmail.com>
2020-07-30 23:30:33 -0300

    Added more test for non owner action

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -89,7 +89,46 @@ defmodule RumblWeb.VideoControllerTest do
       # Assert html reponse was valid, but show the errors
       assert html_response(conn, 200) =~ "check the errors"
       # Confirmes that no video was actually added
-      assert video_count == count_before
+      assert video_count() == count_before
     end
   end
+
+    test "non owner cannot perform any action with video", %{conn: conn} do
+      # Create and add user
+      owner = user_fixture(username: "owner")
+      # Create and add video to previous user
+      video = video_fixture(owner, @create_attrs)
+      # Create a new user to check if can acess the owner videos
+      non_owner = user_fixture(username: "sneaky")
+      # Loggin the non_owner of the video user
+      conn = assign(conn, :current_user, non_owner)
+
+      assert_error_sent(
+        :not_found,
+        fn ->
+          get(conn, Routes.video_path(conn, :show, video))
+        end
+      )
+
+      assert_error_sent(
+        :not_found,
+        fn ->
+          get(conn, Routes.video_path(conn, :edit, video))
+        end
+      )
+
+      assert_error_sent(
+        :not_found,
+        fn ->
+          put(conn, Routes.video_path(conn, :update, video, video: @create_attrs))
+        end
+      )
+
+      assert_error_sent(
+        :not_found,
+        fn ->
+          delete(conn, Routes.video_path(conn, :delete, video))
+        end
+      )
+    end
 end
