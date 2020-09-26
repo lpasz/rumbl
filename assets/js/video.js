a8c996a063772b9ba8d12bbedc96220b1ea3d9f6
lucaspaszinski <lucaspaszinski@gmail.com>
2020-09-26 12:45:24 -0300

    Fixed video channel attribute. Fixed javascript file errors. Removed div id msg-input unused. Changed text area name to id.

--- assets/js/video.js
+++ assets/js/video.js
@@ -27,56 +27,48 @@ let Video = {
         let postButton = document.getElementById( "msg-submit" )
         let videoChannel = socket.channel( "videos:" + videoId )
 
-        // TODO joint the videoChannel
+        // Start listen to click event
+        postButton.addEventListener( "click", e =>
+        {
+            console.log("\n\nMESSAGE INPUT: \n")
+            console.log(msgInput)
+            // create payload obj with 
+            let payload = { body: msgInput.value, at: Player.getCurrentTime() }
+            videoChannel.push( "new_annotation", payload )
+                .receive( "error", e => console.log( e ) )
+            msgInput.value = ""
+        } )
+
+        // on event "new_annotation trigger by click"
+        videoChannel.on( "new_annotation", ( resp ) =>
+        {
+            this.renderAnnotation( msgContainer, resp )
+        } )
+
         videoChannel.join()
-            .receive( "ok", resp => console.log( "joined the video channel", resp ) )
+            .receive( "ok", resp => console.log( "joined te video channel", resp ) )
             .receive( "error", reason => console.log( "join failed", reason ) )
+    },
 
-        videoChannel.on( "ping", ( { count } ) => console.log( "PING", count ) )
+    esc( str )
+    {
+        let div = document.createElement( "div" )
+        div.appendChild( document.createTextNode( str ) )
+        return div.innerHTML
+    },
+
+    renderAnnotation( msgContainer, { user, body, at } )
+    {
+        let template = document.createElement( "div" )
+        template.innerHTML =
+            `<a href="#" data-seek="${ this.esc( at ) }"> 
+                <b>${ this.esc( user.username ) }</b>:${ this.esc( body ) }
+             </a>`
+
+        msgContainer.appendChild( template )
+        msgContainer.scrollTop = msgContainers.scrollHeight
     }
-}
 
-// Create a new channel
-let videoChannel = socket.channel( "videos:" + videoId )
-
-// Start listen to click event
-postButton.addEventListener( "click", e =>
-{
-    let payload = { body: msgInput.value, at: Player.getCurrentTime() }
-    videoChannel.push( "new_annotation", payload )
-        .receive( "error", e => console.log( e ) )
-    msgInput.value = ""
-} )
-
-// on event "new_annotation trigger by click"
-videoChannel.on( "new_annotation", ( resp ) =>
-{
-    this.renderAnnotation( msgContainer, resp )
-} )
-
-videoChannel.join()
-    .receive( "ok", resp => console.log( "joined te video channel", resp ) )
-    .receive( "error", reason => console.log( "join failed", reason ) )
-
-renderAnnotation( msgContainer, { user, body, at } )
-{
-    let template = document.createElement( "div" )
-    template.innerHTML =
-        `<a href="#" 
-            data-seek="${ this.esc( at ) }"> 
-            <b>${ this.esc( user.username ) }
-            </b>:${ this.esc( body ) }
-        </a>`
-
-    msgContainer.appendChild( template )
-    msgContainer.scrollTop = msgContainers.scrollHeight
-}
-
-esc( str )
-{
-    let div = document.createElement( "div" )
-    div.appendChild( document.createTextNode( str ) )
-    return div.innerHTML
 }
 
 export default Video
