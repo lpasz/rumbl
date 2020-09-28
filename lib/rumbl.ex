7fbb2de3e1e91cfbae04afba7c6a7de66f530b46
Lucas Paszinski <lucaspaszinski@gmail.com>
2020-09-28 11:31:17 -0300

    Doc test example

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -4,6 +4,14 @@ defmodule Rumbl do
   and business logic.
 
   Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  if it comes from the database, an external API or others
   """
+
+  @doc """
+   ## Examples
+
+      iex> Rumbl.who_am_i
+      :rumbl
+  """
+  def who_am_i, do: :rumbl
 end
---
+++ test/rumbl_test.exs
@@ -0,0 +1,5 @@
+defmodule RumblTest do
+  use ExUnit.Case, async: true
+
+  doctest Rumbl
+end
